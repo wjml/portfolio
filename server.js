59249d731ebeaee732a9e472d68bcbcc61f227b1
wjml <willian_wjml@hotmail.com>
2019-03-30 21:53:23 -0400

    needed changes to server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static('wjml.herokuapp.com' + '/public'));
+// app.use(express.static('wjml.herokuapp.com' + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-   // ejs render automatically looks in the views folder
    res.sendfile('./index.html');
 });
 
